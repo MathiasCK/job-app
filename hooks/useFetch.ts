06c2a1da7e07ea320e978a7aa256e3067592d104
Mathias Kolberg <Mathias.ck2000@gmail.com>
2024-05-22 13:07:12 +0200

    Create custom useFetch hook

---
+++ hooks/useFetch.ts
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = <T>(
+  endpoint: string,
+  query: {
+    query: string;
+    page?: string;
+    num_pages?: string;
+  },
+): {
+  data: T[];
+  isLoading: boolean;
+  error: string | null;
+  refetch: () => void;
+} => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    params: {
+      ...query,
+    },
+    headers: {
+      "X-RapidAPI-Key": process.env.EXPO_PUBLIC_RAPID_API_KEY,
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+    } catch (error) {
+      alert("There is an error");
+      setError((error as Error).message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
