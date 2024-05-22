35487f061faf087df88c0d0c13583237ed94daff
Mathias Kolberg <Mathias.ck2000@gmail.com>
2024-05-22 12:02:12 +0200

    Import fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,37 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect } from "react";
+import { View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    DMBold: require("~/assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("~/assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("~/assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <Stack />
+    </View>
+  );
 }
